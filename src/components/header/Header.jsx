9f43d07fc22c18aa374b070955ce52a9d510cd08
Valerie <valerie.ielsch@gmail.com>
2022-04-28 18:36:43 +0200

    header done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA.jsx";
+import ME from "../../assets/mePict.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Valérie Ielsch</h1>
+        <h5 className="text-light">Fullstack Developper</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="Valerie"></img>
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+        <HeaderSocials />
+      </div>{" "}
+    </header>
+  );
 };
 
 export default Header;
