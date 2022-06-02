23a4d15e39dfd88d7f8627f452ab1e74d22bec27
Valerie <valerie.ielsch@gmail.com>
2022-06-02 10:25:04 +0200

    add paragraph about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,16 +4,23 @@ import "./about.css";
 const About = () => {
   return (
     <section id="about">
-      <h2>A propos</h2>
+      <h2>Un mot sur moi</h2>
       <div className="text">
-        Lorem Ipsum has been the industry's standard dummy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
-        unchanged. It was popularised in the 1960s with the release of Letraset
-        sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
+        Je glisse un orteil dans le bassin du développement Web via une
+        initiation openClassRoom et me voilà mouillée jusqu'au cou en formation
+        5 mois à la Wild Code School. Je bois parfois la tasse mais je ne coule
+        pas. Cette formation est intensive mais renforce ma perséverance. On
+        aborde une belle dose de concepts, et on se serre les coudes. La Wild
+        c'est aussi un accès à une formation professionnalisante qui nous permet
+        de réaliser notre dernier projet avec un client réel.
+        <br /> Cette aventure se termine bientôt, et avant le grand plongeon,
+        j'aimerais poursuivre mon apprentissage via un contrat d'alternance dans
+        une entreprise.
+        <br />
+        Ici, vous pourrez jeter un oeil à mon CV, Github et à mes projets.
+        <br />
+        J'espère que cela vous donnera envie de me contacter.
+        <br />A bientôt :)
       </div>
     </section>
   );
