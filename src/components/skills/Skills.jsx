9014703894c00ec40e85103db3d7b8021b0c84d8
Valerie <valerie.ielsch@gmail.com>
2024-06-18 16:28:52 +0200

    add colored logos hard skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,6 +1,25 @@
 import React, { useEffect, useRef } from "react";
 import "./skills.css";
 import { motion, useAnimation, useInView } from "framer-motion";
+// import { SiJavascript } from "react-icons/si";
+import { RiJavascriptLine } from "react-icons/ri";
+
+import { SiReact } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiNotion } from "react-icons/si";
+import { FiFigma } from "react-icons/fi";
+import { GrGraphQl } from "react-icons/gr";
+import { SiApollographql } from "react-icons/si";
+import { BiLogoPhp } from "react-icons/bi";
+import { FaSymfony } from "react-icons/fa";
+import { SiSocketdotio } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiTypescript } from "react-icons/si";
+import { TbBrandRedux } from "react-icons/tb";
+import { FaGitAlt } from "react-icons/fa";
+
 const Skills = () => {
   const softSkills = [
     "Curiosité",
@@ -11,16 +30,23 @@ const Skills = () => {
     "Autonomie",
   ];
   const hardSkills = [
-    "Javascript",
-    "React JS",
-    "Node JS",
-    "MySQL",
-    "GitHub",
-    "Notion",
-    "Figma",
-    "Express",
+    <RiJavascriptLine size={60} color="#D6B002" />,
+    <SiReact size={60} color="#5ED2F3" />,
+    <TbBrandRedux size={60} color="#7248B6" />,
+    <SiTypescript size={60} color="#0076C6" />,
+    <FaNodeJs size={60} color="#86BE02" />,
+    <FaSymfony size={60} color="black" />,
+    <BiLogoPhp size={60} color="#31648C" />,
+    <BiLogoPostgresql size={60} color="#31648C" />,
+    <GrMysql size={60} color="#41759B" />,
+    <GrGraphQl size={60} color="#EF0097" />,
+    <SiApollographql size={60} color="black" />,
+    <SiSocketdotio size={60} color="black" />,
+    <FaDocker size={60} color="#036BB0" />,
+    <FaGitAlt size={60} color="#E84F31" />,
+    <FiFigma size={60} color="black" />,
+    <SiNotion size={60} color="black" />,
   ];
-
   const ref = useRef(null);
   const isInView = useInView(ref);
 
